642e77c3fa6609cca09e088d42ee5e1c0afbac8e
arturkeil <kyleartur@gmail.com>
2023-03-21 01:03:05 +0500

    Temp

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AnonymFromInternet/Motel
+
+go 1.19
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func main() {
+	fmt.Println("Motel app say to you")
+
+	newMap := make(map[string][]string)
+	newSlice := []string{
+		"",
+		"",
+	}
 }

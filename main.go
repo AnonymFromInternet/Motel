9f885c4d1c1a2db410ba35eec882c17a56e34ea5
arturkeil <kyleartur@gmail.com>
2023-03-21 16:55:49 +0500

    Temp

--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
-	"io"
+	template2 "html/template"
 	"log"
 	"net/http"
 )
 
+const portNumber = "localhost:8080"
+
+func GetHandlerMainPage(writer http.ResponseWriter, request *http.Request) {
+	renderTemplate(writer, "main")
+}
+
+func GetHandlerContactsPage(writer http.ResponseWriter, request *http.Request) {
+	renderTemplate(writer, "contacts")
+}
+
+func renderTemplate(w http.ResponseWriter, templateRootFileName string) {
+	const extension = ".page.tmpl"
+	parsedTemplate, err := template2.ParseFiles("./templates/" + templateRootFileName + extension)
+
+	if err != nil {
+		log.Println("cannot read parsedTemplate, error :", err)
+
+		return
+	}
+
+	err = parsedTemplate.Execute(w, nil)
+
+	if err != nil {
+		log.Println("cannot execute parsedTemplate to response writer, error :", err)
+
+		return
+	}
+}
+
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		_, err := io.WriteString(writer, "Main page")
+	http.HandleFunc("/main", GetHandlerMainPage)
+	http.HandleFunc("/contacts", GetHandlerContactsPage)
 
-		if err != nil {
-			log.Fatal(err)
-		}
-	})
-
-	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
+	if err := http.ListenAndServe(portNumber, nil); err != nil {
 		log.Fatal("cannot start server. Error :", err)
 	}
 }

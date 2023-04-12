6fd846eef585e1df3c2c216ba7b6e543fbda7ff7
arturkeil <kyleartur@gmail.com>
2023-04-12 22:28:13 +0500

    Adding new postgres functions, handlers, login page template and refactoring.

--- cmd/web/prepareAppDataBeforeRun_test.go
+++ cmd/web/prepareAppDataBeforeRun_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestPrepareAppDataBeforeRun(t *testing.T) {
-	err := prepareAppDataBeforeRunGetDBConnectionPool()
+	err, _ := prepareAppDataBeforeRunGetDBConnectionPool()
 	if err != nil {
 		t.Errorf("[func TestPrepareAppDataBeforeRun] - cannot run function for tests")
 	}
